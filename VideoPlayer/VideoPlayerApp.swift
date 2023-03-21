cd6ea9388e55f44c23205b255f26b9ca906b5de7
jamiecartin <jamie.cartin@icloud.com>
2023-03-21 19:05:48 -0400

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Jamie Cartin on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
