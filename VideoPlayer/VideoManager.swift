ec243e87ee865cdb195d19f756d23c974ea201fc
jamiecartin <jamie.cartin@icloud.com>
2023-03-22 17:01:08 -0400

    create video structure and add json (dummy) data for testing

--- VideoPlayer/VideoManager.swift
+++ VideoPlayer/VideoManager.swift
@@ -10,3 +10,32 @@ import Foundation
 enum Query: String, CaseIterable {
     case nature, animals, people, food, movies, science, tech
 }
+
+struct ResponseBody: Decodable {
+    var page: Int
+    var perPage: Int
+    var totalResults: Int
+    var url: String
+    var videos: [Video]
+}
+
+struct Video: Identifiable, Decodable {
+    var id: Int
+    var image: String
+    var duration: Int
+    var user: User
+    var videoFiles: [VideoFile]
+    
+    struct User: Identifiable, Decodable {
+        var id: Int
+        var name: String
+        var url: String
+    }
+    
+    struct VideoFile: Identifiable, Decodable {
+        var id: Int
+        var quality: String
+        var fileType: String
+        var link: String
+    }
+}
